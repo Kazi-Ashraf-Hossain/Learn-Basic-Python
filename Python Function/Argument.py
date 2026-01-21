9374cb553ad5f0ba1e070fbc15a3ead0872fa5c1
Kazi Ashraf Hossain <ashrafcse020@gmail.com>
2026-01-21 08:24:20 +0600

    Loop and some function code are added

---
+++ Python Function/Argument.py
@@ -0,0 +1,16 @@
+# Tutorial url - https://www.youtube.com/watch?v=ZBNTa9GdY14
+def add(a, b):
+    c = a + b
+    print(c)
+
+add(1, 2)
+
+# Function Return ----
+
+def sum(m, n):
+    o = m + n
+    return o
+
+addition = sum(5  , 2)
+print(addition)
+
---
+++ Python Loops/For Loops.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry", "orange", "strawberry", "Mango", "Pineapple"]
+for x in fruits:
+    # print(x)
+
+    if x == "cherry":    # Continue Statement
+        break
+    print(x)
+"""
+    if x == "Mango": continue   # Break Statement
+    print(x)"""
+
+for r in range(6):
+    print(r)
+
+for br in range(2, 6):
+    print(br)
